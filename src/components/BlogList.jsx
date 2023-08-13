3a9934b0bb77f978c2107edb518cb7b141462518
QDev2k3 <ntbquoc2015@gmail.com>
2023-08-13 15:45:57 +0700

    Add screen for empty blog list

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,6 +1,15 @@
 import '../styles/index.css';
 
 export default function BlogList ({ blogs, title, handleDelete }) {
+    if (blogs.length === 0) {
+        return (
+            <div className='empty-wrapper'>
+                <img src="https://firebasestorage.googleapis.com/v0/b/qblog-26b0a.appspot.com/o/empty_screen.jpg?alt=media&token=2e1b2d33-dfb5-4e9e-bd56-18d9cebd15ca" />
+                <button className='create-button'>Create new blog</button>
+            </div>
+        );
+    }
+    else
     return (
         <div className="list-wrapper">
             <h2>{title}</h2>
@@ -18,5 +27,5 @@ export default function BlogList ({ blogs, title, handleDelete }) {
                 ))}
             </div>
         </div>
-    )
+    );
 }
