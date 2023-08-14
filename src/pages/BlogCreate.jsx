50786c790f9758b36828fd094deefc05becd7fd6
QDev2k3 <ntbquoc2015@gmail.com>
2023-08-14 10:56:06 +0700

    New files

---
+++ src/pages/BlogCreate.jsx
@@ -0,0 +1,9 @@
+import { app, db, collection, addDoc } from '../firebase/firebase';
+
+export default function BlogCreate() {
+    return (
+        <div>
+            <h1>Blog create</h1>
+        </div>
+    )
+}
