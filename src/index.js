3f7d0bc71316320b79feb65bc6a1ffff5cb765de
QDev2k3 <ntbquoc2015@gmail.com>
2023-08-14 10:53:03 +0700

    Add React Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import ErrorPage from './pages/ErrorPage';
+import App from './App';
+import BlogCreate from './pages/BlogCreate';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: '/create',
+        element: <BlogCreate />,
+        errorElement: <ErrorPage />,
+    }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
